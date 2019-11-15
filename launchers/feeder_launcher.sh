0eab2fd35d8902f558994b142e559e59081a930a
Michael Chimento <mchimento@ab.mpg.de>
2019-11-15 16:04:01 +0100

    sleep so network available

--- launchers/feeder_launcher.sh
+++ launchers/feeder_launcher.sh
@@ -3,5 +3,6 @@
 # launches correct python scripts with directory management
 
 cd /home/pi/raspi_tit_scripts
+sleep 10
 python3 feeder_photos.py&
 exit 0
--- launchers/puzzle_launcher.sh
+++ launchers/puzzle_launcher.sh
@@ -2,7 +2,9 @@
 # puzzlelauncher.sh
 # launches correct python scripts with directory management
 
+
 cd /home/pi/raspi_tit_scripts
+sleep 10
 python3 puzzle_video.py&
 python3 rfid_code.py&
 exit 0
