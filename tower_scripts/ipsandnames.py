1ef82c0f9ed460ed4d8e047b74b2e0255a0ea329
Michael Chimento <mchimento@ab.mpg.de>
2019-11-16 11:33:55 +0100

    use # to avoid reading pis

--- tower_scripts/ipsandnames.py
+++ tower_scripts/ipsandnames.py
@@ -4,5 +4,5 @@ import csv
 import socket
 
 with open('../List_of_Cameras.csv') as csvfile:
-	data = csv.reader(csvfile, delimiter=',')
+	data = csv.reader(csvfile, delimiter=',', escapechar="#")
 	pi_data_table = [row for row in data]
