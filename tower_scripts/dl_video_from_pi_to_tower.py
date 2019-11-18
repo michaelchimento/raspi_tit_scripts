68f89494ee88bc53131dbe654805e58e8fce438c
michael <mchimento@ab.mpg.de>
2019-11-18 13:02:10 +0100

    fixed dl_video

--- tower_scripts/dl_video_from_pi_to_tower.py
+++ tower_scripts/dl_video_from_pi_to_tower.py
@@ -4,26 +4,19 @@ import csv
 import datetime as dt
 import os
 from ipsandnames import pi_data_table
+from term_utils import terminal, ping_pi
 
 #reads from csv that has col1:names, col2:IP address (no user!)
 #pi_data_table format is [(pi name1, pi IP1), (pi name2, pi IP2),... etc]
 #print(pi_data_table)
 
-def terminal(command):
-    try:
-        term_output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
-    except subprocess.CalledProcessError as e:
-        print("Oops, something's wrong:")
-        print("{}: {}".format(e.cmd,e.output.decode()))
-    else:
-        return term_output.decode()
-
 ### MOVE VIDEOS TO TRANSFER FOLDER ON PI
 for pi in pi_data_table:  #use this for more than one pi
     print("Transferring videos within Pi for {}".format(pi))
-    command = "ping -c2 {}".format(pi[1])
-    response = terminal(command)
-    if response:
+    reachable = ping_pi(pi[1])
+    if not reachable:
+        print("{} not responding to pings".format(pi[0]))
+    else:
         copy_from = "APAPORIS/MOVED/"
         copy_to = "APAPORIS/TO_TRANSFER/"
 
@@ -39,21 +32,21 @@ for pi in pi_data_table:  #use this for more than one pi
             #move files from "moved" to "to_transfer"
             command = 'ssh pi@{} mv {}* {}'.format(pi[1], copy_from, copy_to)
             terminal(command)
-   
-    else:
-        print("{} not responding to pings".format(pi[0]))
+
+print("***Finished moving files on pis, now moving from Pi to Tower***")
 
 ### COPY VIDEOS AND CSV TO TOWER/DELETE FROM PI
 for pi in pi_data_table:  #use this for more than one pi
-    print("Transferring videos from Pi {} to Desktop".format(pi))
     time_stamp = dt.datetime.now().strftime('%Y-%m-%d_%H')
     target_folder = "{}_{}/".format(pi[0],time_stamp)
-
-    command = "ping -c2 {}".format(pi[1])
-    response = terminal(command)
-    if response:
-        copy_from = "APAPORIS/TO_TRANSFER/"
-        copy_to = "~/TITS/VIDEOS/{}".format(target_folder)
+    copy_from = "APAPORIS/TO_TRANSFER/"
+    copy_to = "~/TITS/VIDEOS/{}".format(target_folder)
+    
+    reachable = ping_pi(pi[1])
+    if not reachable:
+        print("{} not responding to pings".format(pi[0]))
+    else:
+        print("Transferring videos from Pi {} to Desktop".format(pi))
         file_count_TO_TRANSFER = terminal("ssh pi@{} ls {} | wc -l".format(pi[1],copy_from))
         #make folder on this computer
         if int(file_count_TO_TRANSFER) ==0:
@@ -61,15 +54,16 @@ for pi in pi_data_table:  #use this for more than one pi
             pass
         else:
             if not os.path.isdir(copy_to):
+                print("creating folder {}".format(copy_to))
                 command= "mkdir -v {}".format(copy_to)
                 terminal(command)
         
             #create commands to copy files from pi, feeder and observ pi's require recursive scp command
             if "Puzzle" in pi[0] or "Social" in pi[0]:
-                print("Moving {} videos".format(file_count_TO_TRANSFER))
+                print("Moving {} videos".format(file_count_TO_TRANSFER.rstrip()))
                 command = 'scp -p pi@{}:{}* {}'.format(pi[1],copy_from,copy_to)
             else:
-                print("Moving {} folders of photos".format(file_count_TO_TRANSFER))
+                print("Moving {} folders of photos".format(file_count_TO_TRANSFER.rstrip()))
                 command = 'scp -rp pi@{}:{}* {}'.format(pi[1],copy_from,copy_to)
             
             #execute move
@@ -84,8 +78,4 @@ for pi in pi_data_table:  #use this for more than one pi
                 print('All files transferred Successfully')
             else:
                 print("mismatch between file counts of pi and tower")
-                  
-    else:
-        print("{} interrupted during transfer. Not responding to pings".format(pi[0]))
-        break
 
