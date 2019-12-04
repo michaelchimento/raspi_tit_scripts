1ef8b206beae22ab81b2556f4227ec006e65cdbf
Michael Chimento <mchimento@ab.mpg.de>
2019-12-04 19:20:47 +0100

    Update socket_stream.py

--- socket_stream.py
+++ socket_stream.py
@@ -1,6 +1,7 @@
 import socket
 import time
 import picamera
+from camera_settings import *
 
 camera = picamera.PiCamera()
 camera.resolution = "720p"
@@ -12,10 +13,16 @@ server_socket.listen(0)
 
 # Accept a single connection and make a file-like object out of it
 connection = server_socket.accept()[0].makefile('wb')
-try:
+with picamera.PiCamera() as camera:
+    camera.resolution = "720p"
+    camera.framerate = 24
+    camera.brightness = camera_brightness
+    camera.sharpness = camera_sharpness
+    camera.contrast = camera_contrast
+    camera.awb_mode = camera_awb_mode
+    camera.iso = camera_ISO
     camera.start_recording(connection, format='h264')
-    camera.wait_recording(60)
-    camera.stop_recording()
 finally:
+    camera.stop_recording()
     connection.close()
     server_socket.close()
