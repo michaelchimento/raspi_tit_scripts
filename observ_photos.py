b80811ab846ea88e27e9b3e107ec0769874c7d2f
michael <mchimento@ab.mpg.de>
2019-12-05 14:45:29 +0100

    updated sizes

--- observ_photos.py
+++ observ_photos.py
@@ -46,7 +46,7 @@ def make_photos(hour):
         os.mkdir(dir_name)
         camera.annotate_text_size = 15
         camera.annotate_text = datetime.now().strftime('%Y-%m-%d %H:%M:%S:%f')
-        resize_tuple = (int(resize_scale*camera.resolution[0]),int(resize_scale*camera.resolution[1]))
+        resize_tuple = (camera.resolution[0],int(.5*camera.resolution[1]))
         for i, filename in enumerate(camera.capture_continuous("{}/{}_".format(dir_name,filenamePrefix)+"{timestamp:%Y-%m-%d-%H-%M-%S-%f}.jpg", resize = resize_tuple)):
             camera.annotate_text = datetime.now().strftime('%Y-%m-%d %H:%M:%S:%f')
             if i == 599:
