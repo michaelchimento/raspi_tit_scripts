8910c0cb1995444d8ac0f84b98691fe5919a699e
michael <mchimento@ab.mpg.de>
2020-02-09 12:34:09 +0100

    camera settings

--- camera_settings.py
+++ camera_settings.py
@@ -19,20 +19,20 @@ camera_awb_mode = 'tungsten'
 camera_sharpness = 25
 camera_contrast = 25
 feeder_start = 7
-feeder_end = 17
+feeder_end = 18
 social_start = 11
 social_end = 16
 puzzle_start = 7
-puzzle_end = 17
+puzzle_end = 18
 observ_start = 7
-observ_end = 17
+observ_end = 18
 sensitivity_value = 200
 
 def set_exposure_shutter(hour):
     if hour < 8:
         camera_exposure_mode = 'auto'
         camera_shutter_speed = 5000    
-    if hour < 10:
+    elif hour < 10 and hour >=8:
         camera_exposure_mode = 'auto'
         camera_shutter_speed = 4000
     elif hour >= 10 and hour < 15:
