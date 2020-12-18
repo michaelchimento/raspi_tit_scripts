9fd3b4e6626f653f8591a725501b5a4cc409e7af
Michael Chimento <mchimento@ab.mpg.de>
2020-12-18 15:08:26 +0100

    Update IR_test.py

--- tests/IR_test.py
+++ tests/IR_test.py
@@ -1,21 +1,11 @@
 import RPi.GPIO as IO
 IO.setwarnings(False)
 IO.setmode (IO.BCM)
-from rpi_info import name
 
-
-if "P3" in name:
-    left_IR_pin=19
-    print("changing left_push pin to {}".format(left_IR_pin))
-else:
-    left_IR_pin=23
-
-if "P8" in name:
-    right_IR_pin=19
-    print("changing right_push pin to {}".format(right_IR_pin))
-
-else:
-    right_IR_pin=24
+left_IR_pin=23
+right_IR_pin=24
+IO.setup(left_IR_pin,IO.IN) #blue solve
+IO.setup(right_IR_pin,IO.IN) #red solve
 
 
 while 1:
