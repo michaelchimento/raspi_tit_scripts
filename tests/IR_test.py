960ad55f3d7b12728e512162b371fb4ecc1f3c92
Michael Chimento <mchimento@ab.mpg.de>
2020-12-18 14:48:02 +0100

    Update IR_test.py

--- tests/IR_test.py
+++ tests/IR_test.py
@@ -3,13 +3,13 @@ IO.setwarnings(False)
 IO.setmode (IO.BCM)
 if "P3" in name:
     left_IR_pin=19
-    print("changing left_push pin to {}".format(left_IR_pin)
+    print("changing left_push pin to {}".format(left_IR_pin))
 else:
     left_IR_pin=23
 
 if "P8" in name:
     right_IR_pin=19
-    print("changing right_push pin to {}".format(right_IR_pin)
+    print("changing right_push pin to {}".format(right_IR_pin))
 
 else:
     right_IR_pin=24
