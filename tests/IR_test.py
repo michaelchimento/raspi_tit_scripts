5dda15619612950d13641f3b381a7dcb2e673040
Michael Chimento <mchimento@ab.mpg.de>
2021-02-04 15:51:44 +0100

    Update IR_test.py

--- tests/IR_test.py
+++ tests/IR_test.py
@@ -1,8 +1,12 @@
 import RPi.GPIO as IO
+from rpi_info import name
 IO.setwarnings(False)
 IO.setmode (IO.BCM)
 
-left_IR_pin=23
+if "Puzzle_P4" in name:
+    left_IR_pin=22
+else:
+    left_IR_pin=23
 right_IR_pin=24
 IO.setup(left_IR_pin,IO.IN) #blue solve
 IO.setup(right_IR_pin,IO.IN) #red solve
